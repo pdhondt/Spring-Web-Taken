7e12fc72800555b4b691272f5a5a73b127776f16
Peter D'hondt <dhondt.peter@gmail.com>
2023-02-13 12:17:41 +0100

    taak 1.7 - Bug fixes

--- src/test/java/be/vdab/cinefest/controllers/FilmControllerTest.java
+++ src/test/java/be/vdab/cinefest/controllers/FilmControllerTest.java
@@ -8,8 +8,8 @@ import org.springframework.test.context.junit4.AbstractTransactionalJUnit4Spring
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Sql("/films.sql")
--- src/test/resources/films.sql
+++ src/test/resources/films.sql
@@ -1,2 +1,2 @@
 insert into films(titel, jaar, vrijePlaatsen, aankoopprijs)
-values ('Top Gun', 1986, 5, 450.00), ('Lord of the Rings', 2001, 8, 800.00)
+values ('Top Gun', 1986, 5, 450.00), ('Lord of the Rings', 2001, 8, 800.00);

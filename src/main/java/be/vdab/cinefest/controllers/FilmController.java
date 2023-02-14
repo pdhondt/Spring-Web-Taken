e3670f84bab436aa236d4d99cad5a9716450b07b
Peter D'hondt <dhondt.peter@gmail.com>
2023-02-14 11:27:52 +0100

    taak 1.8 - DTO

--- src/main/java/be/vdab/cinefest/controllers/FilmController.java
+++ src/main/java/be/vdab/cinefest/controllers/FilmController.java
@@ -13,13 +13,19 @@ class FilmController {
     FilmController(FilmService filmService) {
         this.filmService = filmService;
     }
+    private record IdTitelJaarVrijePlaatsen(long id, String titel, int jaar, int vrijePlaatsen) {
+        IdTitelJaarVrijePlaatsen(Film film) {
+            this(film.getId(), film.getTitel(), film.getJaar(), film.getVrijePlaatsen());
+        }
+    }
     @GetMapping("films/totaalvrijeplaatsen")
     long findTotaalVrijePlaatsen() {
         return filmService.findTotaalVrijePlaatsen();
     }
     @GetMapping("films/{id}")
-    Film findById(@PathVariable long id) {
+    IdTitelJaarVrijePlaatsen findById(@PathVariable long id) {
         return filmService.findById(id)
+                .map(film -> new IdTitelJaarVrijePlaatsen(film))
                 .orElseThrow(() -> new FilmNietGevondenException(id));
     }
 }
